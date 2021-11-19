c7dd243624bde9cddc1a97d9117e32024381e760
Matt McCarley <mdmccarley89@gmail.com>
2021-11-19 14:05:05 -0800

    Add initial files

---
+++ Solution/CustomerManager.java
@@ -0,0 +1,43 @@
+package com.amazon.ata.lambdaexpressions.prework.consumer;
+
+import com.amazon.ata.lambdaexpressions.prework.resources.consumer.DatabaseManager;
+
+import java.util.List;
+
+/**
+ * Responsible for checking which customers have accepted the new terms
+ * and conditions, emailing those who have not.
+ */
+public class CustomerManager {
+
+    /**
+     * Main method of project. When completed it should print to standard output a list of emailed customers.
+     * @param args Main method parameter
+     */
+    public static void main(String[] args) {
+        CustomerManager customerManager = new CustomerManager();
+        List<String> emailedCustomers = customerManager.checkCustomers();
+        for (String customer : emailedCustomers) {
+            System.out.println("Emailed " + customer);
+        }
+    }
+
+    /**
+     * Checks if customers have accepted new terms and conditions.
+     * @return List of Strings of customers emailed.
+     */
+    public List<String> checkCustomers() {
+        DatabaseManager databaseManager = new DatabaseManager();
+        // TODO Get Customers from DatabaseManager using the method getCustomers
+        List<String> customers = databaseManager.getCustomers();
+
+        // TODO Use List's forEach() method with a lambda expression to check the status for each customer,
+        //  emailing each customer who has not accepted the new terms.
+        customers.forEach(customer -> {
+            if (!databaseManager.checkCustomer(customer)) {
+                databaseManager.emailCustomer(customer);
+            }
+        });
+        return databaseManager.getEmailedCustomers();
+    }
+}
---
+++ Solution/hints/hint_01.md
@@ -0,0 +1,3 @@
+`List` contains a method called `forEach` that iterates through all items in a `List`. Inside the method call for 
+`forEach` you must put your implementing code. The lambda expression should be written in-line, not declared to a 
+variable.
